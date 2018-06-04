cb104fbe48ee4ec810c535fedc313502107a6c1b
eduardocmm <38039767+eduardocmm@users.noreply.github.com>
2018-06-04 10:27:18 -0300

    Create P7.c

---
+++ P7.c
@@ -0,0 +1,26 @@
+\\Eduardo Costa Mendes      
+\\11721ETE010
+
+#include <stdio.h>
+
+int Ack(m,n){
+    switch(m){
+        case 0: 
+        return n+1;
+        default:
+            if(n==0)
+               return Ack(m-1,1);
+            else
+               return Ack(m-1,Ack(m,n-1));
+    }
+}
+
+int main()
+{   
+    int m, n;
+    printf("Digite os dois numeros inteiros da funcao de Ackermann: ");
+    scanf("%d", &m); getchar();
+    scanf("%d", &n); getchar();
+    printf("\n\nO resultado de %d e %d foi: %d",m, n, Ack(m,n));
+    return 0;
+}
